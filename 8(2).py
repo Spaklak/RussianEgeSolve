ec51028f5ac7928c002669779447c186ba0b225f
Spaklak <kru4aew.egor@yandex.ru>
2022-12-25 17:25:56 +0300

    Создание строки

--- 8(2).py
+++ 8(2).py
@@ -9,6 +9,7 @@ count = 0
 for i in set(itertools.product(s, repeat=5)): # используется, если у нас не важно кол-во букв. И может быть ииииии
     pass
 for i in set(itertools.permutations(s, 4)):# если переставляет буквы в слове( 5 за кол-во букв)
+    a = ''.join(i) # так получается строка
     count += 1
-    print(i)
+    print(a)
 print(count)
