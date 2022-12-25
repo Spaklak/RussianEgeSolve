43e6fb37e2648b1f6ee548e11d47ab100d7a9b63
Spaklak <kru4aew.egor@yandex.ru>
2022-12-25 17:23:42 +0300

    commit to 8(2)

--- 8(2).py
+++ 8(2).py
@@ -6,7 +6,7 @@ import itertools
 
 s = 'игорь'
 count = 0
-for i in (itertools.product(s, repeat=5)): # используется, если у нас не важно кол-во букв. И может быть ииииии
+for i in set(itertools.product(s, repeat=5)): # используется, если у нас не важно кол-во букв. И может быть ииииии
     pass
 for i in set(itertools.permutations(s, 4)):# если переставляет буквы в слове( 5 за кол-во букв)
     count += 1
