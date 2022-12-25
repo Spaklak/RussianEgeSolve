f3f38944bf79b19e2d84714f022cdf587e666f18
Spaklak <kru4aew.egor@yandex.ru>
2022-12-25 17:19:05 +0300

    гайд на itertools

---
+++ 8(2).py
@@ -0,0 +1,14 @@
+'''
+Разбор решения с помощью itertools
+'''
+
+import itertools
+
+s = 'игорь'
+count = 0
+for i in (itertools.product(s, repeat=5)): # используется, если у нас не важно кол-во букв. И может быть ииииии
+    pass
+for i in set(itertools.permutations(s, 4)):# если переставляет буквы в слове( 5 за кол-во букв)
+    count += 1
+    print(i)
+print(count)
